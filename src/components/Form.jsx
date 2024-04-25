f2d271a213e8446f334fe6b95ab10a549ba5a17a
Imran Hossain Sakil <imranhossainsakil22@gmail.com>
2024-04-25 16:28:16 +0600

    simple ref and ref callback syntax and map

---
+++ src/components/Form.jsx
@@ -0,0 +1,18 @@
+
+import { useRef } from 'react';
+export default function Form() {
+
+  const inputRef = useRef(null)
+  const handleFocusInput =() => {
+    inputRef.current.focus()
+  }
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleFocusInput}>
+        Focus the input
+      </button>
+    </>
+  );
+}
